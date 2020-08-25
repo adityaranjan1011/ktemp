c7c55a6f9725c3910ac5ef566c0718294cbbe447
adityranjan1011 <adityamnnit009@gmail.com>
2020-08-25 18:42:51 +0530

    API subscribed

--- src/app/services/dashboard.service.ts
+++ src/app/services/dashboard.service.ts
@@ -10,13 +10,9 @@ export class DashboardService {
   public uploadDocumentDetails = new BehaviorSubject<any>('');
 
   constructor(private http: HttpClient,) {
-    this.getJSON().subscribe(data => {
-  });
+   
    }
-  
-   public getJSON(): Observable<any> {
-    return this.http.get("./assets/data.json");
-}
+   
    getDataset():Observable<any>{
      return this.http.get(`https://pf-stage.koshex.com/v1/GenerateStats/`);
    }
